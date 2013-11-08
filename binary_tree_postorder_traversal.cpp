e54f6e3f9ba69acc71ab33e75dd40952e56fcdc2
Xiangli Liao <liaoxl2012@gmail.com>
2013-11-08 22:01:46 +0800

    Update my leetcode cpp files

--- binary_tree_postorder_traversal.cpp
+++ binary_tree_postorder_traversal.cpp
@@ -13,55 +13,55 @@
 using namespace std;
 
 struct TreeNode{
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution{
-	public:
-		vector<int> preorderTraversal(TreeNode *root)
-		{
-			vector<int> res;
-			if(root==NULL) return res;
-			stack<TreeNode *> myqueue;
-			stack<int> myres;
+    public:
+        vector<int> preorderTraversal(TreeNode *root)
+        {
+            vector<int> res;
+            if(root==NULL) return res;
+            stack<TreeNode *> myqueue;
+            stack<int> myres;
 
-			myqueue.push(root);
+            myqueue.push(root);
 
-			while(!myqueue.empty())
-			{
-				TreeNode *p=myqueue.top();
-				myres.push(p->val);
-				myqueue.pop();
-				if(p->left!=NULL)
-				{
-					myqueue.push(p->left);
-				}
-				if(p->right!=NULL)
-				{
-					myqueue.push(p->right);
-				}
-			}
+            while(!myqueue.empty())
+            {
+                TreeNode *p=myqueue.top();
+                myres.push(p->val);
+                myqueue.pop();
+                if(p->left!=NULL)
+                {
+                    myqueue.push(p->left);
+                }
+                if(p->right!=NULL)
+                {
+                    myqueue.push(p->right);
+                }
+            }
 
-			while(!myres.empty())
-			{
-				res.push_back(myres.top());
-				myres.pop();
-			}
-			return res;
-		}
+            while(!myres.empty())
+            {
+                res.push_back(myres.top());
+                myres.pop();
+            }
+            return res;
+        }
 };
 
 int main(int argc, char* argv[])
 {
-	TreeNode root(2);
-	root.left=new TreeNode(1);
-	root.right=new TreeNode(4);
+    TreeNode root(2);
+    root.left=new TreeNode(1);
+    root.right=new TreeNode(4);
 
-	Solution s;
-	s.preorderTraversal(&root);
-	return 0;
+    Solution s;
+    s.preorderTraversal(&root);
+    return 0;
 }
 
